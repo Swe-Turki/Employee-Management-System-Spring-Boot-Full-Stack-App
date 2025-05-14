29bea4f9fd0b6bcf59067a474bf395c3a551de0c
TURKI ALOTAIBI <119834796+Swe-Turki@users.noreply.github.com>
2025-05-14 20:09:50 +0300

    Delete Employee Management System By Spring Boot Full-Stack App directory

--- Employee Management System By Spring Boot Full-Stack App/employee_management/src/main/java/com/example/employee_management/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.employee_management.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
